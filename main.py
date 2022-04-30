1a7311ce7469dceb24d2ea964d4b7a1bbaeb3b0f
Arsenii Chudaiev <62509307+CHUDILOO@users.noreply.github.com>
2022-04-30 17:18:12 +0300

    туц туц

--- main.py
+++ main.py
@@ -36,14 +36,26 @@ window = display.set_mode((win_width, win_height))
 window.fill(back)
 display.set_caption("Ping pong")
 
+
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
 
-racket1 = Player("racket1.png",30,200,12,120,4)
-racket2 = Player("racket2.png",550,200,12,120,4)
+racket1 = Player("racket1.png",30,200,12,120,16)
+racket2 = Player("racket2.png",550,200,12,120,16)
 ball = GameSprite("ball.png",200,200,50,50,4)
+
+speed_x = 3
+speed_y = 4
+
+score1 = 0
+score2 = 0
+
+font.init()
+font = font.Font(None, 35)
+score = font.render("0 : 0", True, (0, 0, 0))
+lose = font.render("", True, (180,0,0))
 
 
 while game:
@@ -55,9 +67,38 @@ while game:
             window.fill(back)
             racket1.update_1()
             racket2.update_2()
+            ball.rect.x += speed_x
+            ball.rect.y += speed_y
 
+            if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+                speed_x *= -1
+                speed_y *= 1
+
+            if ball.rect.y > win_height - 50 or ball.rect.y < 0 :
+                speed_y *= -1
+
+            if ball.rect.x < 0:
+                score2 += 1
+                ball.rect.x, ball.rect.y = 200, 200
+
+            if ball.rect.x > win_width - 50 :
+                score1 += 1
+                ball.rect.x, ball.rect.y = 200, 200
+            score = font.render(f"{score1} : {score2}", True, (0,0,0))  
             racket1.reset()
             racket2.reset()
+            ball.reset()
+
+            if score1 > 10:
+                lose = font.render("PLAYER 2 LOX!", True, (180,0,0))
+                finish = True
+                window.blit(lose, (250,200))
+
+            if score2 > 10:
+                lose = font.render("PLAYER 1 LOX!", True, (180,0,0))
+                finish = True
+                window.blit(lose, (250,200))
+ 
 
     display.update()
     clock.tick(FPS)
